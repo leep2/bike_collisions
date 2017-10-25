b11f14fc4b7ed6375583558186ae8cb7e7446130
Lee <leepang15@gmail.com>
2017-10-25 07:14:14 -0700

    Separated into functions

--- main.py
+++ main.py
@@ -3,113 +3,131 @@ import requests
 import credentials
 import numpy as np
 from stravalib import client
-import polyline
 import pickle
 import bisect
 import gmplot
 
-df = pd.read_csv('CollisionRecords.txt')
+def read_collision_data():
+    df = pd.read_csv('CollisionRecords.txt')
+    df = df[df['BICYCLE_ACCIDENT'] == 'Y']
+    return df
 
-df['intersection'] = (df['PRIMARY_RD'].str.split() + ['and'] + df['SECONDARY_RD'].str.split() + ['san', 'francisco']).str.join('+')
-intersection = df['intersection'].values
+def collision_intersections(df):
+    df['intersection'] = (df['PRIMARY_RD'].str.split() + ['and'] + df['SECONDARY_RD'].str.split() + ['san', 'francisco']).str.join('+')
+    inters = df['intersection'].values
+    inters[110] = 'valencia+st+and+18th+st+san+francisco'
+    inters[216] = 'de+young+museum+san+francisco'
+    inters[226] = 'funston+ave+and+fulton+st+san+francisco'
+    inters[243] = 'fulton+st+and+6th+ave+san+francisco'
+    inters[315] = 'clipper+st+san+francisco'
+    inters[328] = 'columbus+ave+and+filbert+st+san+francisco'
+    inters[463] = '7th+st+san+francisco'
+    inters[537] = 'willard+st+san+francisco'
+    inters[605] = 'fort+point+san+francisco'
+    for i in range(607, 612):
+        inters[i] = 'fort+point+san+francisco'
+    return inters, len(inters)
 
-for i, x in enumerate(intersection):
-    if '/' in x:
-        print i, x
+def google_api_geo(inters, num_collisions):
+    geo = np.zeros([num_collisions, 2])
+    for i in range(num_collisions):
+        r = requests.get('https://maps.googleapis.com/maps/api/place/textsearch/json?query=' + inters[i] + '&key=' + credentials.key['google'])
+        location = r.json()['results'][0]['geometry']['location']
+        geo[i][0] = location['lat']
+        geo[i][1] = location['lng']
+    np.savetxt('collision_geo.csv', geo, delimiter = ',')
+    return geo
 
-intersection[110] = 'valencia+st+and+18th+st+san+francisco'
-intersection[216] = 'de+young+museum+san+francisco'
-intersection[226] = 'funston+ave+and+fulton+st+san+francisco'
-intersection[243] = 'fulton+st+and+6th+ave+san+francisco'
-intersection[315] = 'clipper+st+san+francisco'
-intersection[328] = 'columbus+ave+and+FILBERT+ST+san+francisco'
-intersection[463] = '7th+st+san+francisco'
-intersection[537] = 'willard+st+san+francisco'
-intersection[605] = 'fort+point+san+francisco'
-for i in xrange(607, 612):
-    intersection[i] = 'fort+point+san+francisco'
+def bounding_box(left, bottom, right, top):
+    return {'min_lon': left, 'min_lat': bottom, 'max_lon': right, 'max_lat': top}
 
-incidents = len(intersection)
-lat_lon = np.zeros([incidents, 2])
-for i in range(incidents):
-    r = requests.get('https://maps.googleapis.com/maps/api/place/textsearch/json?query=' + intersection[i] + '&key=' + credentials.key['google'])
-    location = r.json()['results'][0]['geometry']['location']
-    lat_lon[i][0] = location['lat']
-    lat_lon[i][1] = location['lng']
+def lat_lon_grid(bounds, height, width):
+    latitude_grid = np.linspace(bounds['min_lat'], bounds['max_lat'], height + 1)
+    longitude_grid = np.linspace(bounds['min_lon'], bounds['max_lon'], width + 1)
+    return latitude_grid, longitude_grid
 
-with open('incident_list.pkl', 'wb') as fp:
-    pickle.dump(lat_lon, fp)
+def grid(geo, latitude_grid, longitude_grid):
+    grid_arr = np.zeros([len(geo), 2])
+    for i, g in enumerate(geo):
+        grid_arr[i][0] = bisect.bisect(latitude_grid, g[0]) - 1
+        grid_arr[i][1] = bisect.bisect(longitude_grid, g[1]) - 1
+    return grid_arr
 
-lat_grid = np.linspace(37.706, 37.814, 5)
-lon_grid = np.linspace(-122.519, -122.354, 7)
+def strava_api_segments(bounds, height, width):
+    strava_client = client.Client(access_token = credentials.key['strava'])
+    latitude_grid, longitude_grid = lat_lon_grid(bounds, height, width)
+    segment_list = []
+    for x in range(len(latitude_grid) - 1):
+        for y in range(len(longitude_grid) - 1):
+            explore = strava_client.explore_segments([latitude_grid[x], longitude_grid[y], latitude_grid[x + 1], longitude_grid[y + 1]])
+            for segment in explore:
+                segment_list.append(segment.id)
+    seg_obj_list = []
+    for id in segment_list:
+        seg = strava_client.get_segment(id)
+        seg_obj_list.append(seg)
+    num_segments = len(seg_obj_list)
+    segment_detail = np.zeros([num_segments, 4])
+    for i, segment in enumerate(seg_obj_list):
+        segment_detail[i][0] = segment.start_latlng[0]
+        segment_detail[i][1] = segment.start_latlng[1]
+        segment_detail[i][2] = (segment.updated_at - segment.created_at).days
+        segment_detail[i][3] = segment.effort_count
+    np.savetxt('segment_detail.csv', segment_detail, delimiter = ',')
+    return segment_detail
 
-incident_grid = np.zeros([incidents, 2])
-for i in range(incidents):
-    incident_grid[i][0] = bisect.bisect(lat_grid, lat_lon[i][0]) - 1
-    incident_grid[i][1] = bisect.bisect(lon_grid, lat_lon[i][1]) - 1
+def effort_geo_to_grid(grid, eff):
+    return np.concatenate([grid, eff[:, 2:]], axis = 1)
 
-df_incident = pd.DataFrame(incident_grid)
-df_incident[2] = 1
+def combine_collision_effort(col, eff):
+    df_collision = pd.DataFrame(col, columns = ['y', 'x'])
+    df_collision['count'] = 1
+    collision_count = df_collision.groupby(['y', 'x'], as_index = False).count()
+    df_effort = pd.DataFrame(eff, columns = ['y', 'x', 'days', 'efforts'])
+    df_effort['adj_efforts'] = df_effort['efforts'] * 365.0 / df_effort['days']
+    df_effort = df_effort[['y', 'x', 'adj_efforts']]
+    max_effort = df_effort.groupby(['y', 'x'], as_index = False).max()
+    mean_effort = df_effort.groupby(['y', 'x'], as_index = False).mean()
+    collision_max = collision_count.merge(max_effort, left_on = ['y', 'x'], right_on = ['y', 'x'])
+    collision_max_mean = collision_max.merge(mean_effort, left_on = ['y', 'x'], right_on = ['y', 'x'])
+    collision_max_mean.columns = ['y', 'x', 'count', 'max', 'mean']
+    collision_max_mean['col_max'] = collision_max_mean['count'] / collision_max_mean['max']
+    collision_max_mean['col_mean'] = collision_max_mean['count'] / collision_max_mean['mean']
+    return collision_max_mean
 
-strava = client.Client(access_token = credentials.key['strava'])
+def color_map(df, column, bounds, latitude_grid, longitude_grid):
+    color_progression = ['Green', 'YellowGreen', 'Yellow', 'Orange', 'OrangeRed', 'Red']
+    num_colors = len(color_progression)
+    low = df[column].min()
+    high = df[column].max()
+    df['rel_value'] = (df[column] * 1.0 - low) / (high - low)
+    df['color'] = (df['rel_value'] * num_colors).astype(int).clip(upper = num_colors - 1)
+    c_map = df[['y', 'x', 'color']].astype(int).values
+    center_lat = (bbox['min_lat'] + bbox['max_lat']) * 1.0 / 2
+    center_lon = (bbox['min_lon'] + bbox['max_lon']) * 1.0 / 2
+    gmap = gmplot.GoogleMapPlotter(center_lat, center_lon, 13)
+    for row in c_map:
+        s = latitude_grid[row[0]]
+        n = latitude_grid[row[0] + 1]
+        w = longitude_grid[row[1]]
+        e = longitude_grid[row[1] + 1]
+        gmap.polygon([s, n, n, s], [w, w, e, e], color = color_progression[row[2]])
+    gmap.draw("mymap.html")
 
-segment_list = []
-for x in range(len(lat_grid) - 1):
-    for y in range(len(lon_grid) - 1):
-        explore = strava.explore_segments([lat_grid[x], lon_grid[y], lat_grid[x + 1], lon_grid[y + 1]])
-        for e in explore:
-            segment_list.append(e.id)
-
-with open('segment_list.pkl', 'wb') as fp:
-    pickle.dump(segment_list, fp)
-
-seg_obj_list = []
-for s in segment_list:
-    seg = strava.get_segment(s)
-    seg_obj_list.append(seg)
-
-effort = np.zeros([len(seg_obj_list), 3])
-for i in range(len(seg_obj_list)):
-    effort[i][0] = bisect.bisect(lat_grid, seg_obj_list[i].start_latlng[0]) - 1
-    effort[i][1] = bisect.bisect(lon_grid, seg_obj_list[i].start_latlng[1]) - 1
-    adj_year = 365.0 / (seg_obj_list[i].updated_at - seg_obj_list[i].created_at).days
-    effort[i][2] = seg_obj_list[i].effort_count * adj_year
-
-df_effort = pd.DataFrame(data = effort)
-mean_effort = df_effort.groupby([0, 1], as_index = False).mean()
-sum_effort = df_effort.groupby([0, 1], as_index = False).sum()
-max_effort = df_effort.groupby([0, 1], as_index = False).max()
-
-incident_group = df_incident.groupby([0, 1], as_index = False).count()
-
-incident_mean = incident_group.merge(mean_effort, left_on = [0, 1], right_on = [0, 1])
-incident_mean.columns = [0, 1, 'inc', 'mean']
-incident_max = incident_mean.merge(max_effort, left_on = [0, 1], right_on = [0, 1])
-incident_max.columns = [0, 1, 'inc', 'mean', 'max']
-incident_sum = incident_max.merge(sum_effort, left_on = [0, 1], right_on = [0, 1])
-incident_sum.columns = [0, 1, 'inc', 'mean', 'max', 'sum']
-incident_sum['inc_mean'] = incident_sum['inc'] / incident_sum['mean']
-incident_sum['inc_max'] = incident_sum['inc'] / incident_sum['max']
-incident_sum['inc_sum'] = incident_sum['inc'] / incident_sum['sum']
-
-color_progression = ['Green', 'YellowGreen', 'Yellow', 'Orange', 'OrangeRed', 'Red']
-total_colors = len(color_progression)
-inc_max_max = incident_sum['inc_max'].max()
-inc_max_min = incident_sum['inc_max'].min()
-incident_sum['rel_inc_max'] = (incident_sum['inc_max'] * 1.0 - inc_max_min) / (inc_max_max - inc_max_min)
-incident_sum['color'] = (incident_sum['rel_inc_max'] * total_colors).astype(int).clip(upper = total_colors - 1)
-
-choropleth = incident_sum[[0, 1, 'color']].astype(int).values
-
-center_lat = (37.706 + 37.814) / 2
-center_lon = (-122.519 - 122.354) / 2
-gmap = gmplot.GoogleMapPlotter(center_lat, center_lon, 13)
-
-for row in choropleth:
-    s = lat_grid[row[0]]
-    n = lat_grid[row[0] + 1]
-    w = lon_grid[row[1]]
-    e = lon_grid[row[1] + 1]
-    gmap.polygon([s, n, n, s], [w, w, e, e], color = color_progression[row[2]])
-
-gmap.draw("mymap.html")
+if __name__ == '__main__':
+    '''
+    collisions = read_collision_data()
+    intersections, num_collisions = collision_intersections(collisions)
+    collision_geo = google_api_geo(intersections, num_collisions)
+    '''
+    collision_geo = np.loadtxt('collision_geo.csv', delimiter = ',')
+    bbox = bounding_box(-122.516, 37.707, -122.356, 37.813)
+    lat_grid, lon_grid = lat_lon_grid(bbox, 4, 6)
+    collision_grid = grid(collision_geo, lat_grid, lon_grid)
+#    effort = strava_api_segments(bbox, 4, 6)
+    effort = np.loadtxt('segment_detail.csv', delimiter = ',')
+    e_grid = grid(effort, lat_grid, lon_grid)
+    effort_grid = effort_geo_to_grid(e_grid, effort)
+    collision_effort = combine_collision_effort(collision_grid, effort_grid)
+    color_map(collision_effort, 'col_max', bbox, lat_grid, lon_grid)
+    print collision_effort
